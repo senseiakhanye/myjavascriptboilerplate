af29b857191ce4fcbdb3cc42fb7bbe779423c7d4
Katleho <akhanye@student.fr.42>
2019-12-20 05:37:07 +0200

    Express running and installed nodemon for testing purposes

---
+++ buildScripts/staticServer.js
@@ -0,0 +1,17 @@
+const express = require('express');
+const chalk = require('chalk');
+const path = require('path');
+const app = express();
+const port = process.env.PORT || 3000;
+const srcFolder = path.join(__dirname, "../src/");
+
+console.log(srcFolder);
+
+app.use(express.static(srcFolder));
+
+app.listen(port, (err) => {
+  if (err != null) {
+    return console.log(chalk.red(`Error running port ${port} error message ${err}`));
+  }
+  console.log(chalk.green(`Listening to port ${port}`));
+})
